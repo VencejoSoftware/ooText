b4648156849d350afa3cce50f60cbbd45e8e865e
Vencejo Software <vencejosoftware@gmail.com>
2017-12-06 15:46:41 -0300

    Fix status texts

--- batch/get_dependencies.bat
+++ batch/get_dependencies.bat
@@ -1,17 +1,17 @@
 @echo off
 
 if not exist %delphiooLib%\ooBatch\ (
-  @echo "Clonning dxDUnit..."
+  @echo "Clonning ooBatch..."
   git clone https://github.com/VencejoSoftware/ooBatch.git %delphiooLib%\ooBatch\
   call %delphiooLib%\ooBatch\code\get_dependencies.bat
 )
 
 if not exist %delphi3rdParty%\dcpcrypt\ (
-  @echo "Clonning dunit-agiledoxtestrunner..."
+  @echo "Clonning dcpcrypt..."
   git clone https://github.com/VencejoSoftware/DCPCrypt.git %delphi3rdParty%\dcpcrypt\
 )
 
 if not exist %delphiooLib%\ooGeneric\ (
-  @echo "Clonning dunit-agiledoxtestrunner..."
+  @echo "Clonning ooGeneric..."
   git clone https://github.com/VencejoSoftware/ooGeneric.git %delphiooLib%\ooGeneric\
 )
